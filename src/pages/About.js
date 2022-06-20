35519976cb3b104f982eea426359339e526e5c97
ihateindex <47144148+ihateindex@users.noreply.github.com>
2022-06-20 18:49:06 +0900

    0620 - About 페이지 프로토타입 추가

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,47 @@ import Header from '../components/Header';
 function About(props) {
     return (
         <div id="container" className={props.pageName}>
-            <Header color="black"/>
+            <div className="inner">
+                <Header color="black" />
+                <div className="title-block">
+                    <h3 className="sub-title">Who we are</h3>
+                    <h2 className="title">
+                        크리에이티브가 전략입니다<br></br>아이디어가 무기입니다
+                    </h2>
+                </div>
+                <div className="video-block"></div>
+                <div className="do-block">
+                    <div className="inner">
+                        <div className="title-block">
+                            <h3 className="title">What we do</h3>
+                        </div>
+                        <div className="box"></div>
+                    </div>
+                </div>
+                <div className="team-block">
+                    <div className="inner">
+                        <div className="title-block">
+                            <h3 className="title">Team</h3>
+                        </div>
+                        <div className="box">
+                            <ul>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className="client-block">
+                    <div className="inner">
+                        <div className="title-block">
+                            <h3 className="title">Client</h3>
+                        </div>
+                        <div className='box'></div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
