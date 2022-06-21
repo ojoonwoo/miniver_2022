162eae954c49c9e604129cb28c279519fc0ad5fb
ihateindex <47144148+ihateindex@users.noreply.github.com>
2022-06-21 11:37:18 +0900

    0621 - Contact 작업중

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,14 +4,22 @@ function Contact(props) {
     return (
         <div id="container" className={props.pageName}>
             <Header></Header>
-            <div className='inner'>
-                <div className='title-block'>
-                    <h3 className='title-num'>1.</h3>
-                    <h2 className='title'>어떤 프로젝트를 문의하고 싶으신가요?</h2>
-                    <p className='title-desc'>(중복 선택이 가능합니다.)</p>
+            <div className="inner">
+                <div className="title-block">
+                    <h3 className="title-num">1.</h3>
+                    <h2 className="title">어떤 프로젝트를 문의하고 싶으신가요?</h2>
+                    <p className="title-desc">(중복 선택이 가능합니다.)</p>
                 </div>
-                <div className='select-block'>
-                    <h1>셀렉트블록</h1>
+                <div className="select-block">
+                    <div className="inner">
+                        <button>Compaign Site</button>
+                        <button>Viral Video</button>
+                        <button>Social media</button>
+                        <button>Short Video</button>
+                        <button>Goods</button>
+                        <button>Photography</button>
+                        <input type="text" placeholder="입력하시오" />
+                    </div>
                 </div>
             </div>
         </div>
