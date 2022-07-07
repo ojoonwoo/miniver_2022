e4c5b9ba0e8aa6cacd598d1241b9c09f76408de2
ojw <ojoonwoo@gmail.com>
2022-07-07 18:21:47 +0900

    0707 - 어드민 수정

--- admin/application/libs/config.php
+++ admin/application/libs/config.php
@@ -3,11 +3,11 @@
 /*
 @todo : 실서버 경로에 맞춰 분기 핋요
 */
-define('_MNV_ROOT', $_SERVER['DOCUMENT_ROOT'].'/miniver-2022/');
-define('_ROOT', $_SERVER['DOCUMENT_ROOT'].'/miniver-2022/admin/');
+define('_MNV_ROOT', $_SERVER['DOCUMENT_ROOT'].'/miniver_2022/');
+define('_ROOT', $_SERVER['DOCUMENT_ROOT'].'/miniver_2022/admin/');
 define('_VIEW_DIR', _ROOT.'application/views/');
-define('_PUBLIC_URL', 'http://'.$_SERVER['HTTP_HOST'].'/miniver-2022/admin/public/');
-define('_ROOT_URL', 'http://'.$_SERVER['HTTP_HOST'].'/miniver-2022/admin/');
+define('_PUBLIC_URL', 'http://'.$_SERVER['HTTP_HOST'].'/miniver_2022/admin/public/');
+define('_ROOT_URL', 'http://'.$_SERVER['HTTP_HOST'].'/miniver_2022/admin/');
 
 define('_WORK_UPLOAD_URL', 'http://'.$_SERVER['HTTP_HOST'].':3000/works/');
 // define('_DBTYPE', 'mysql');
