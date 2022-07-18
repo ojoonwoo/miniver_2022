eddbb33a00b123345a6eed4a6e05c5229be07265
ojw <ojoonwoo@gmail.com>
2022-07-18 18:44:06 +0900

    0718 - merged / framer transition

--- src/components/WorkBox.js
+++ src/components/WorkBox.js
@@ -16,32 +16,29 @@ function WorkBox(props) {
     }, []);
 
     return (
-        <div className={`workbox ${styles.workbox} ${styles[props.thumb]}`}>
-            <Link to={`/project/${props.item.idx}`} className={`workbox ${styles.workbox}`}>
-            
-                <div className={styles.wrapper}>
-                    <div className={styles['item-img']}>
-                        {props.thumb==='square'
-                        ?
-                            <img src={`/works/${props.item.idx}/thumb_square/${props.item.thumb_square}`}></img>
-                        :
-                            <img src={`/works/${props.item.idx}/thumb_rectangle/${props.item.thumb_rectangle}`}></img>
-                        } 
-                    </div>
-                    <div className={styles['box-overlay']}>
-                        <div className={styles['logo-img']} style={{backgroundImage: `url(/works/${props.item.idx}/logo_img/${props.item.logo_img})`}}></div>
-                    </div>
+        <Link to={`/project/${props.item.idx}`} className={`workbox ${styles.workbox} ${styles[props.thumb]}`}>
+            <div className={styles.wrapper}>
+                <div className={styles['item-img']}>
+                    {props.thumb==='square'
+                    ?
+                        <img src={`/works/${props.item.idx}/thumb_square/${props.item.thumb_square}`}></img>
+                    :
+                        <img src={`/works/${props.item.idx}/thumb_rectangle/${props.item.thumb_rectangle}`}></img>
+                    } 
                 </div>
-                {
-                props.desc ?
-                <div className={`workbox__desc ${styles['workbox-desc']}`}>
-                    <p className={styles['client-name']}>{props.item.client_name}</p>
-                    <span className={styles.title}>{props.item.work_title}</span>
-                </div> :
-                ""
-                }
-            </Link>
-        </div>
+                <div className={styles['box-overlay']}>
+                    <div className={styles['logo-img']} style={{backgroundImage: `url(/works/${props.item.idx}/logo_img/${props.item.logo_img})`}}></div>
+                </div>
+            </div>
+            {
+            props.desc ?
+            <div className={`workbox__desc ${styles['workbox-desc']}`}>
+                <p className={styles['client-name']}>{props.item.client_name}</p>
+                <span className={styles.title}>{props.item.work_title}</span>
+            </div> :
+            ""
+            }
+        </Link>
     );
 }
 
