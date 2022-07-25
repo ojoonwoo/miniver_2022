516ecb9f8eb77d2663923f46389dd81d43fee31a
ihateindex <47144148+ihateindex@users.noreply.github.com>
2022-07-25 12:09:09 +0900

    0725 - contact 작업중

---
+++ src/components/ArrowRight.js
@@ -0,0 +1,9 @@
+function ArrowRight() {
+    return (
+            <svg className="arrow__right" xmlns="http://www.w3.org/2000/svg">
+                <path d="M1.08289 9.99997H48.3746L39.6315 1.08228" stroke="white" strokeWidth="2" strokeLinecap="round" />
+            </svg>
+    );
+}
+
+export default ArrowRight;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,15 +5,23 @@ import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
 import { changeColor } from './../store.js';
 import PageTransition from '../components/PageTransition';
+import ArrowRight from '../components/ArrowRight.js';
 
 function Contact(props) {
     let themeColor = useSelector((state) => {
         return state.themeColor;
     });
-    console.log(themeColor);
+
+    let contactState = useSelector((state) => {
+        return state.contactState;
+    });
+    console.log(contactState);
     let dispatch = useDispatch();
 
     const [categoryData, setCategoryData] = useState([]);
+    console.log(categoryData);
+
+    const [checkedList, setCheckedList] = useState([]);
 
     useEffect(() => {
         console.log('컨텍트 마운트');
@@ -29,7 +37,7 @@ function Contact(props) {
             method: 'get',
             url: '/api/work/getcategories',
         });
-        
+
         setCategoryData(result.data.list);
         console.log(result.data.list);
     };
@@ -41,23 +49,43 @@ function Contact(props) {
             <div id="container" className={props.pageName}>
                 {/* <Header></Header> */}
                 <div className="inner">
-                    <div className="title-block">
-                        <h3 className="title-num">1.</h3>
-                        <h2 className="title">어떤 프로젝트를 문의하고 싶으신가요?</h2>
-                        <p className="title-desc">(중복 선택이 가능합니다.)</p>
-                    </div>
-                    <div className="select-block">
-                        <div className="inner">
-                            {categoryData.map((item) => 
-                                <button key={item.idx}>{item.category_name}</button>
-                            )}
-                            {/* <button>Compaign Site</button>
+                    <div className="surveys">
+                        <div className="survey-item"></div>
+                        <div className="title-block">
+                            <h3 className="title-num">1.</h3>
+                            <h2 className="title">
+                                프로젝트를
+                                <br />
+                                문의하고 싶으신가요?
+                            </h2>
+                            <p className="title-desc">* 중복 선택</p>
+                        </div>
+                        <div className="input-block">
+                            <div className="inner">
+                                {categoryData.map((item) => (
+                                    <div className="input-wrap" key={item.idx}>
+                                        <label>
+                                            <input type="checkbox" value={item.category_name} />
+                                            <span>{item.category_name}</span>
+                                        </label>
+                                    </div>
+                                ))}
+                                {/* <button>Compaign Site</button>
                             <button>Viral Video</button>
                             <button>Social Media</button>
                             <button>Short Video</button>
                             <button>Goods</button>
                             <button>Photography</button> */}
-                            <input type="text" placeholder="입력하시오" />
+                                <div className="input-wrap">
+                                    <label>
+                                        <input type="checkbox" />
+                                        <span>기타</span>
+                                    </label>
+                                </div>
+                                <button>
+                                    <ArrowRight/>
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
