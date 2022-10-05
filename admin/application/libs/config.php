b0e9bc1a001ecae499220039da1602400dbebb69
ojw <ojoonwoo@gmail.com>
2022-10-05 11:33:05 +0900

    1005 - 배포 작업중

--- admin/application/libs/config.php
+++ admin/application/libs/config.php
@@ -7,19 +7,29 @@ if(_SERVICE_ENV === 'DEV') {
 
     define('_MNV_ROOT', $_SERVER['DOCUMENT_ROOT'].'/miniver_2022/');
     define('_ROOT', $_SERVER['DOCUMENT_ROOT'].'/miniver_2022/admin/');
+    define('_VIEW_DIR', _ROOT.'application/views/');
+    define('_PUBLIC_URL', 'http://'.$_SERVER['HTTP_HOST'].'/miniver_2022/admin/public/');
+    define('_ROOT_URL', 'http://'.$_SERVER['HTTP_HOST'].'/miniver_2022/admin/');
+
+    define('_WORK_UPLOAD_URL', 'http://'.$_SERVER['HTTP_HOST'].':3000/works/');
+    define('_WORK_UPLOAD_DIR', _MNV_ROOT.'uploads/work/');
+    define('_PRESS_UPLOAD_URL', 'http://'.$_SERVER['HTTP_HOST'].':3000/journalists/');
+    define('_PRESS_UPLOAD_DIR', _MNV_ROOT.'uploads/press/');
 } else {
     define('_MNV_ROOT', '/home/miniver_2022/');
     define('_ROOT', $_SERVER['DOCUMENT_ROOT'].'/');
+    define('_VIEW_DIR', _ROOT.'application/views/');
+    define('_PUBLIC_URL', 'http://'.$_SERVER['HTTP_HOST'].'/public/');
+    define('_ROOT_URL', 'http://'.$_SERVER['HTTP_HOST'].'/');
+
+    define('_WORK_UPLOAD_URL', 'http://dev.minivertising.kr/works/');
+    define('_WORK_UPLOAD_DIR', _MNV_ROOT.'uploads/work/');
+    define('_PRESS_UPLOAD_URL', 'http://dev.minivertising.kr/journalists/');
+    define('_PRESS_UPLOAD_DIR', _MNV_ROOT.'uploads/press/');
 }
 
-define('_VIEW_DIR', _ROOT.'application/views/');
-define('_PUBLIC_URL', 'http://'.$_SERVER['HTTP_HOST'].'/miniver_2022/admin/public/');
-define('_ROOT_URL', 'http://'.$_SERVER['HTTP_HOST'].'/miniver_2022/admin/');
 
-define('_WORK_UPLOAD_URL', 'http://'.$_SERVER['HTTP_HOST'].':3000/works/');
-define('_WORK_UPLOAD_DIR', _MNV_ROOT.'uploads/work/');
-define('_PRESS_UPLOAD_URL', 'http://'.$_SERVER['HTTP_HOST'].':3000/journalists/');
-define('_PRESS_UPLOAD_DIR', _MNV_ROOT.'uploads/press/');
+
 
 
 session_start();
