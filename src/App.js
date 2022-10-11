acb0a8a515caa285ee9f334fd5c6ca87d801bda4
ihateindex <47144148+ihateindex@users.noreply.github.com>
2022-10-11 17:40:57 +0900

    1011 - 모바일 컨텍트페이지 높이 값 이슈 수정중

--- src/App.js
+++ src/App.js
@@ -38,9 +38,6 @@ function App() {
             // console.log('윈도우 리사이즈');
             winWidth = window.innerWidth;
 
-            let vh = window.innerHeight * 0.01;
-            document.documentElement.style.setProperty('--vh', `${vh}px`);
-
             if (winWidth >= 1200) {
                 dispatch(setDevice('desktop'));
             } else {
@@ -48,9 +45,6 @@ function App() {
             }
         });
     }, []);
-
-    // let vh = window.innerHeight * 0.01;
-    // document.documentElement.style.setProperty('--vh', `${vh}px`);
 
     return (
         <div className="App" data-theme={themeColor}>
