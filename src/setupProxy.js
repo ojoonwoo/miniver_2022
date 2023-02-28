0b6a40d6a9c65f63e7e4db5b4f23fd84b94eaa2f
ojw <ojoonwoo@gmail.com>
2023-02-28 15:38:22 +0900

    0228 - https 설정

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,21 +4,21 @@ module.exports = function(app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'http://localhost:4000',
+            target: 'https://localhost:4000',
             changeOrigin: true,
         })
     );
     app.use(
         '/works',
         createProxyMiddleware({
-            target: 'http://localhost:4000',
+            target: 'https://localhost:4000',
             changeOrigin: true,
         })
     );
     app.use(
         '/journalists',
         createProxyMiddleware({
-            target: 'http://localhost:4000',
+            target: 'https://localhost:4000',
             changeOrigin: true,
         })
     );
