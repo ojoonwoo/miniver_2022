f5c218d8de39ce3d16cf27b9c7adc670cae4cd8d
ihateindex <47144148+ihateindex@users.noreply.github.com>
2023-05-23 16:40:27 +0900

    0523 - 어드민 블로그 add 기능 최적화중

--- admin/application/controllers/BlogController.php
+++ admin/application/controllers/BlogController.php
@@ -49,55 +49,15 @@ class BlogController extends Controller
 
         $model = new \application\models\BlogModel();
         $blog_id = $model->getLastBlogID();
-        // ! 임시 코드
-        if (!$blog_id) {
-            $blog_id = 1;
-        }
+        $blog_id = $blog_id ?: 1;
 
         $json = file_get_contents('php://input');
-        // $blogdata = json_decode($json);
-
         $data = json_decode($json, true);
         $imageData = $data['imageData'];
-        // print_r($imageData);
 
-        // base 64인 파일 저장
-        foreach ($imageData as $key => &$value) {
-            // print_r($value['data']['url']);
-            $imageDataUrl = $value['data']['url'];
-            $parts = explode('base64,', $imageDataUrl);
-            if (count($parts) > 1) {
-                $base64_image_str = $parts[1]; // 'base64,' 이후 부분
-                $imageDataUrl = $base64_image_str;
-                $imageDataUrl = str_replace(' ', '+', $imageDataUrl);
-                $imageDataUrl = base64_decode($imageDataUrl);
-                // echo $imageDataUrl;
-                $file_save_object = $this->save_image($imageDataUrl, $blog_id);
-                if ($file_save_object['url']) {
-                    $value['data']['url'] = $file_save_object['url'];
-                } else {
-                    echo "file_save_object error";
-                }
-            } else {
-                echo "The string does not contain 'base64,'";
-            }
-        }
+        $imageDataMapped = $this->decodeImageUrls($data, $imageData, $blog_id);
 
-        // 에디터 데이터와 이미지 데이터의 ID 값 매칭을 위해 변환
-        $imageDataMapped = [];
-        foreach ($imageData as $item) {
-            $imageDataMapped[$item['id']] = $item;
-        }
-
-        // 이제 array1에서 각 블록을 순회하면서 ID가 일치하는 imageData의 항목을 찾습니다.
-        foreach ($data['editorData']['blocks'] as &$block1) {
-            if (isset($imageDataMapped[$block1['id']])) {
-                // ID가 일치하는 항목이 imageData에 있다면, URL을 대체합니다.
-                $block1['data']['url'] = $imageDataMapped[$block1['id']]['data']['url'];
-            }
-        }
-
-        $insert_result = $model->insertBlog($data);
+        $insert_result = $model->insertBlog($imageDataMapped);
 
         if ($insert_result) {
             // $url = _ROOT_URL . 'blog';
@@ -110,6 +70,38 @@ class BlogController extends Controller
             // echo "<script>location.replace('$url')</script>";
             echo $insert_result;
         }
+    }
+    private function decodeImageUrls($data, $imageData, $blog_id)
+    {
+        $imageDataMapped = [];
+
+        foreach ($imageData as $key => &$value) {
+            $parts = explode('base64,', $value['data']['url']);
+
+            // If the string does not contain 'base64,', simply skip this iteration
+            if (count($parts) <= 1) {
+                continue;
+            }
+
+            $base64_image_str = $parts[1]; // Get the part after 'base64,'
+            $decodedImage = base64_decode(str_replace(' ', '+', $base64_image_str));
+            $file_save_object = $this->save_image($decodedImage, $blog_id);
+
+            if ($file_save_object['url']) {
+                $value['data']['url'] = $file_save_object['url'];
+                $imageDataMapped[$value['id']] = $value;
+            }
+        }
+
+        // 이제 $data['editorData']의 각 blocks들을 순회하면서 ID가 일치하는 imageData의 항목을 찾습니다.
+        foreach ($data['editorData']['blocks'] as &$block) {
+            if (isset($imageDataMapped[$block['id']])) {
+                // ID가 일치하는 항목이 imageData에 있다면, URL을 대체합니다.
+                $block['data']['url'] = $imageDataMapped[$block['id']]['data']['url'];
+            }
+        }
+
+        return $data;
     }
     private function save_image($image, $blog_id)
     {
@@ -142,7 +134,8 @@ class BlogController extends Controller
     {
         return md5(uniqid(rand(), true));
     }
-    public function itemEdit($blog_id) {
+    public function itemEdit($blog_id)
+    {
         if ($_SERVER['REQUEST_METHOD'] !== "POST") {
             exit();
         }
