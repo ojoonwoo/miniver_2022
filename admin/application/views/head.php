46e071261ea8c5ba9e7d58b77649109d5a579252
ihateindex <47144148+ihateindex@users.noreply.github.com>
2023-05-30 17:05:27 +0900

    0530 - BlogBox 코드 리팩토링

--- admin/application/views/head.php
+++ admin/application/views/head.php
@@ -9,38 +9,40 @@
 // }
 
 
-if($page_title == '') {
+if ($page_title == '') {
     $pg_title = "MINIVERTISING";
 } else {
     $pg_title = $page_title;
 }
 ?>
 <html lang="ko">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?=_PUBLIC_URL?>lib/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= _PUBLIC_URL ?>lib/bootstrap/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
-    <link rel="stylesheet" href="<?=_PUBLIC_URL?>css/app.css">
+    <link rel="stylesheet" href="<?= _PUBLIC_URL ?>css/app.css">
     <?php
-    if($this->page_css_name) {
+    if ($this->page_css_name) {
     ?>
-        <link rel="stylesheet" href="<?=_PUBLIC_URL?>css/<?=$this->page_css_name?>.css">
+        <link rel="stylesheet" href="<?= _PUBLIC_URL ?>css/<?= $this->page_css_name ?>.css">
     <?php
     }
     ?>
-    <script src="<?=_PUBLIC_URL?>lib/jquery-3.3.1.min.js"></script>
+    <script src="<?= _PUBLIC_URL ?>lib/jquery-3.3.1.min.js"></script>
     <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
     <!-- <script src="https://cdn.jsdelivr.net/npm/@editorjs/editorjs@latest"></script> -->
-    <script src="<?=_PUBLIC_URL?>lib/editorjs.umd.js"></script>
+    <script src="<?= _PUBLIC_URL ?>lib/editorjs.umd.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@editorjs/header@latest"></script>
     <script src="https://cdn.jsdelivr.net/npm/@editorjs/image@2.3.0"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/@editorjs/paragraph@2.9.0/dist/bundle.min.js"></script> -->
     <script src="https://cdn.jsdelivr.net/npm/simple-image-editorjs@latest"></script>
     <title><?php echo $pg_title ?></title>
     <script>
-        var _root_url = "<?=_ROOT_URL?>";
+        var _root_url = "<?= _ROOT_URL ?>";
     </script>
 </head>
+
 <body>
-    
--- src/components/BlogBox.js
+++ src/components/BlogBox.js
@@ -1,76 +1,70 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import styles from './BlogBox.module.scss';
 import { Link } from 'react-router-dom';
 
-function BlogBox(props) {
-    // console.log(props);
-    // console.log(props.item.blog_register_date);
-    let rawBlogDate = new Date(props.item.blog_register_date);
+function BlogBox({item}) {
 
-    const year = rawBlogDate.getFullYear();
-    const month = rawBlogDate.toLocaleString('en-US', { month: 'long' });
-    const day = rawBlogDate.getDate();
+    // * React의 destructuring을 사용하여 props.item 객체에서 필요한 속성들을 바로 추출해서 변수 선언.
+    const {
+        blog_title, 
+        blog_register_date, 
+        blog_json, 
+        blog_color, 
+        blog_writer, 
+        idx
+    } = item;
 
-    const convertedDate = `${month} ${day}, ${year}`;
-    // console.log(convertedDate);
+    const editorData = JSON.parse(blog_json);
 
-    const [title, setTitle] = useState('');
-    const [firstImage, setFirstImage] = useState('');
-    const [textHeader, setTextHeader] = useState('');
-    const [iconColor, setIconColor] = useState('');
-    const [authorName, setAuthorName] = useState('');
+    // * useMemo를 사용하여, 최적화. 기존은 useEffect안에서 useState로 값을 넣었으나 리렌더링등의 상황에서 대상 값이 변하지 않더라도 대상도 리렌더링되서 불필요한 렌더가 발생.
+    // * useMemo는 useMemo 마지막 부분의 []안에 들어있는 대상의 값이 변하지 않는 이상 리렌더링하지않기때문에 불필요한 렌더가 사라짐.
+    const dateFormatted = useMemo(() => {
+        let rawBlogDate = new Date(blog_register_date);
+        const year = rawBlogDate.getFullYear();
+        const month = rawBlogDate.toLocaleString('en-US', { month: 'long' });
+        const day = rawBlogDate.getDate();
+        return `${month} ${day}, ${year}`;
+    }, [blog_register_date]);
+
+    const firstImage = useMemo(() => {
+        const firstImageObj = editorData.blocks.find(block => block.type === 'image');
+        return firstImageObj ? firstImageObj.id : null;
+    }, [editorData]);
+
+    const textHeader = useMemo(() => {
+        const textHeaderObj = editorData.blocks.find(block => block.type === 'header');
+        const textDescObj = editorData.blocks.find(block => block.type === 'paragraph');
+        return textHeaderObj ? textHeaderObj.data.text : textDescObj.data.text;
+    }, [editorData]);
 
     useEffect(() => {
-        setTitle(props.item.blog_title);
-        const editorData = JSON.parse(props.item.blog_json);
-        const firstImageObj = editorData.blocks.find(item => item.type === 'image');
-        if(firstImageObj) {
-            setFirstImage(firstImageObj.id);
-        }
-        const textHeaderObj = editorData.blocks.find(item => item.type === 'header')
-        const textDescObj = editorData.blocks.find(item => item.type === 'paragraph')
-        if(textHeaderObj) {
-            setTextHeader(textHeaderObj.data.text);
-        } else {
-            setTextHeader(textDescObj.data.text);
-        }
-        setIconColor(props.item.blog_color);
-        setAuthorName(props.item.blog_writer);
-
-
-        // console.log(editorData);
-        // console.log(firstImage);
-        // console.log(textHeader);
+        // console.log('마운트');
+        return () => {
+            // console.log('언마운트');
+        };
     }, []);
-    // useEffect(() => {
-    //     console.log(editorData);
-    //     setBlocks(editorData.blocks);
-    //     firstImage = editorData.blocks.find(item => item.type === 'image');
-    //     console.log(firstImage);
-    // }, [editorData]);
 
     return (
-        // <Link to={"/blog/"+props.item.idx} className={styles.blogbox}>
-        <Link to={`/blog/${props.item.idx}`} className={styles.blogbox}>
+        // * 기존은 'props.item.idx'였으나 위에서 변수 선언해두었기때문에 'idx'로 축약됨.
+        <Link to={`/blog/${idx}`} className={styles.blogbox}>
             <div className={styles['title-block']}>
-                <p className={styles['date']}>{convertedDate}</p>
-                <p className={styles['title']}>{title}</p>
+                <p className={styles['date']}>{dateFormatted}</p>
+                <p className={styles['title']}>{blog_title}</p>
             </div>
             {firstImage &&
             <div className={styles['img-block']}>
-                <img className={styles['thumb']} src={`/postings/${props.item.idx}/${firstImage}.png`} alt=""></img>
-                {/* <img className={styles['thumb']} src={`/postings/1/WpllNiiTlM.png`} alt=""></img> */}
+                <img className={styles['thumb']} src={`/postings/${idx}/${firstImage}.png`} alt=""></img>
             </div>
             }
             <div className={styles['text-block']}>
                 <div className={styles['author-box']}>
                     <p className={styles['by']}>by</p>
-                    <div className={styles['icon']} style={{backgroundColor: iconColor}}>
+                    <div className={styles['icon']} style={{backgroundColor: blog_color}}>
                         <svg viewBox="0 0 8.5 10.2">
                             <path d="M8.5,0v10.2H6.1V3.3l-1,4.6H3.4l-1-4.4v6.7H0V0h3.6c0.1,0.6,0.7,4.1,0.7,4.1L4.9,0H8.5L8.5,0z" />
                         </svg>
                     </div>
-                    <p className={styles['name']}>{authorName}</p>
+                    <p className={styles['name']}>{blog_writer}</p>
                 </div>
                 {textHeader &&
                 <p className={styles['desc']}>
