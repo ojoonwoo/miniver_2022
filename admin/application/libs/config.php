5837aec2e7758588349e5398823b0ab9a8a04d11
ojw <ojoonwoo@gmail.com>
2023-06-12 10:56:42 +0900

    0612 - blog dev

--- admin/application/libs/config.php
+++ admin/application/libs/config.php
@@ -21,14 +21,14 @@ if(_SERVICE_ENV === 'DEV') {
     define('_MNV_ROOT', '/var/www/html/miniver_2022/');
     define('_ROOT', $_SERVER['DOCUMENT_ROOT'].'/');
     define('_VIEW_DIR', _ROOT.'application/views/');
-    define('_PUBLIC_URL', 'https://'.$_SERVER['HTTP_HOST'].'/public/');
-    define('_ROOT_URL', 'https://'.$_SERVER['HTTP_HOST'].'/');
+    define('_PUBLIC_URL', 'http://'.$_SERVER['HTTP_HOST'].'/public/');
+    define('_ROOT_URL', 'http://'.$_SERVER['HTTP_HOST'].'/');
 
     define('_WORK_UPLOAD_URL', 'https://minivertising.kr/works/');
     define('_WORK_UPLOAD_DIR', _MNV_ROOT.'uploads/work/');
     define('_PRESS_UPLOAD_URL', 'https://minivertising.kr/journalists/');
     define('_PRESS_UPLOAD_DIR', _MNV_ROOT.'uploads/press/');
-    define('_BLOG_UPLOAD_URL', 'https://'.$_SERVER['HTTP_HOST'].':3000/postings/');
+    define('_BLOG_UPLOAD_URL', 'https://minivertising.kr/postings/');
     define('_BLOG_UPLOAD_DIR', _MNV_ROOT.'uploads/blog/');
 }
 define('_BASE_UPLOAD_DIR', _MNV_ROOT.'uploads/');
